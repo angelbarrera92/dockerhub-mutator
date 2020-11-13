a07d4ae3985252e22b9ae3f13de59260bd141f88
Ángel Barrera Sánchez <angel@sighup.io>
2020-11-13 20:56:21 +0100

    Mutate all pods specs objects

--- mutation.py
+++ mutation.py
@@ -1,6 +1,7 @@
 import base64
 
 import jsonpatch
+from docker_image.reference import Reference
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -11,13 +12,124 @@ def healthcheck():
     return "ok"
 
 
-@app.route("/mutator/deployment", methods=['POST'])
-def mutate_deployment():
+@app.route("/mutator/dockerhub", methods=['POST'])
+def mutate_dockerhub():
     request_info = request.get_json()
     uid = request_info["request"]["uid"]
-    image_name = request_info["request"]["object"]["spec"]["template"]["spec"]["containers"][0]["image"]
-    new_image = f"mirror.gcr.io/library/{image_name}"
-    return admission_response_patch(uid, "Mutating container dockerhub image", json_patch=jsonpatch.JsonPatch([{"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": new_image}, {"op": "add", "path": "/metadata/labels/mutated", "value": "yes"}]))
+    obj = request_info["request"]["object"]
+    kind = obj["kind"]
+    patches = list()
+    if kind == "Pod":
+        patches = pod(obj)
+    elif kind == "Deployment":
+        patches = deployment(obj)
+    elif kind == "Job":
+        patches = job(obj)
+    elif kind == "CronJob":
+        patches = cronjob(obj)
+    elif kind == "ReplicaSet":
+        patches = replicaset(obj)
+    elif kind == "ReplicationController":
+        patches = replicationController(obj)
+    elif kind == "DaemonSet":
+        patches = daemonset(obj)
+    elif kind == "StatefulSet":
+        patches = statefulset(obj)
+    elif kind == "PodTemplate":
+        patches = podtemplate(obj)
+    if len(patches) > 0:
+        patches.append(
+            {"op": "add", "path": "/metadata/labels/mutated", "value": "yes"})
+    return admission_response_patch(uid, "Mutating containers images from dockerhub", json_patch=jsonpatch.JsonPatch(patches))
+
+
+def is_dockerhub_image(image):
+    ref = Reference.parse_normalized_named(image)
+    hostname, name = ref.split_hostname()
+    if "docker.io" in hostname:
+        return True
+    return False
+
+
+def replace_docker_io(image):
+    ref = Reference.parse_normalized_named(image)
+    return ref.string().replace("docker.io", "mirror.gcr.io")
+
+
+def pod(obj):
+    patches = []
+    index = 0
+    containers = obj["spec"]["containers"]
+    for container in containers:
+        image = container["image"]
+        if is_dockerhub_image(image):
+            patches.append(
+                {"op": "replace", "path": f"/spec/containers/{index}/image", "value": replace_docker_io(image)})
+        index += 1
+    return patches
+
+
+def _generic_template(obj):
+    patches = []
+    index = 0
+    containers = obj["spec"]["template"]["spec"]["containers"]
+    for container in containers:
+        image = container["image"]
+        if is_dockerhub_image(image):
+            patches.append(
+                {"op": "replace", "path": f"/spec/template/spec/containers/{index}/image", "value": replace_docker_io(image)})
+        index += 1
+    return patches
+
+
+def deployment(obj):
+    return _generic_template(obj)
+
+
+def job(obj):
+    return _generic_template(obj)
+
+
+def replicaset(obj):
+    return _generic_template(obj)
+
+
+def daemonset(obj):
+    return _generic_template(obj)
+
+
+def statefulset(obj):
+    return _generic_template(obj)
+
+
+def replicationController(obj):
+    return _generic_template(obj)
+
+
+def cronjob(obj):
+    patches = []
+    index = 0
+    containers = obj["spec"]["jobTemplate"]["spec"]["template"]["spec"]["containers"]
+    for container in containers:
+        image = container["image"]
+        if is_dockerhub_image(image):
+            patches.append(
+                {"op": "replace", "path": f"/spec/jobTemplate/spec/template/spec/containers/{index}/image", "value": replace_docker_io(image)})
+        index += 1
+    return patches
+
+
+def podtemplate(obj):
+    patches = []
+    index = 0
+    containers = obj["template"]["spec"]["containers"]
+    for container in containers:
+        image = container["image"]
+        if is_dockerhub_image(image):
+            patches.append(
+                {"op": "replace", "path": f"/template/spec/containers/{index}/image", "value": replace_docker_io(image)})
+        index += 1
+    return patches
 
 
 def admission_response_patch(uid, message, json_patch):
